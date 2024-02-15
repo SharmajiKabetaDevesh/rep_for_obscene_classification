814957ce4a4eb9084c79fc4955e5ed4161c21903
Devesh <deveshrs2016@gmail.com>
2024-02-15 21:23:49 +0530

    dev

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6078e858af2b22eb00384a65b225c8a473f1e471333c758d444f9cb5a8cacb0a
+size 1703
---
+++ classifier.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c2b015b80e6b39ecd53de19ffdceacc590741757d2c163646f70a6f48bc2c9a9
+size 2639
